03677eb5cb045fabaa0ad845ab26ab27e1c08be3
77681649 <77681649@qq.com>
2016-06-27 20:24:43 +0800

    tyosssss
    add commit

--- react-redux/react-redux-master/src/utils/wrapActionCreators.js
+++ react-redux/react-redux-master/src/utils/wrapActionCreators.js
@@ -1,5 +1,10 @@
 import { bindActionCreators } from 'redux'
 
+/**
+ * 包绕ActionCreater
+ * @param {Object} actionCreaters 动作创建者
+ * @returns {Object}
+ */
 export default function wrapActionCreators(actionCreators) {
   return dispatch => bindActionCreators(actionCreators, dispatch)
 }
